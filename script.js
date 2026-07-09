2b5d1bcda136c851594b73b12589e4a9232350a5
sdncibinong370-prog <sdncibinong370@gmail.com>
2026-07-09 14:58:26 +0700

    Add form submission handler with success message

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("formMutasi").addEventListener("submit", function(e){
 
+e.preventDefault();
+
+document.getElementById("hasil").innerHTML =
+"✅ Data berhasil dicek.<br>Langkah berikutnya kita akan menghubungkan ke Google Spreadsheet.";
+
+});
